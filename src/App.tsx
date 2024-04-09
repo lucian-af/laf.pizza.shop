0836f61e5a1754c615ed4927e0513e8dae155b22
Lucian AF <lucian_af@hotmail.com>
2024-04-09 17:33:09 -0300

    chore: instala e configura libs shadcn/ui e tailwindcss;

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {  
   return (
-    <h1>Pizz.Shop</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Hello, World!</Button>
+    </div>
   )
 }
