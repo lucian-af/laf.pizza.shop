dbd6be8d12fb6bc0dd7d4f20263198e1a26e95cc
Lucian AF <lucian_af@hotmail.com>
2024-04-11 19:54:57 -0300

    chore(react-helmet-async): adiciona lib para SEO das páginas;

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>,
 )
