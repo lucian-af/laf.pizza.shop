b8cd25a1d954c7fdbd0e713dd9564f527c1c3600
Lucian AF <lucian_af@hotmail.com>
2024-05-03 22:08:40 -0300

    test(restaurant): adiciona rota para cadastro de restaurantes;

--- pizzashop-api/tests/PizzaShop.Tests/Domain/RestaurantTest.cs
+++ pizzashop-api/tests/PizzaShop.Tests/Domain/RestaurantTest.cs
@@ -7,14 +7,24 @@ namespace PizzaShop.Tests.Domain
 {
 	public class RestaurantTest
 	{
-		[Fact(DisplayName = "[Domain.Restaurant] - Não deve permitir criar um Restaurant para um usuário 'Customer'")]
-		public void Restaurant_NaoDeveCriarRestaurantComUserDoTipoCustomer()
+		[Fact(DisplayName = "[Domain.Restaurant] - Não deve permitir alterar um manager de um Restaurant para um usuário 'Customer'")]
+		public void Restaurant_NaoDevePermitirAlterarRestaurantParaUserDoTipoCustomer()
 		{
 			var faker = new Faker("pt_BR");
-			var user = new User(faker.Person.FullName, faker.Person.Email, faker.Phone.PhoneNumber(), RoleUser.Customer);
+			var user = new User(
+				 faker.Person.FullName,
+				 faker.Person.Email,
+				  faker.Phone.PhoneNumber(),
+				   RoleUser.Customer);
 
-			var exception = Assert.Throws<DomainException>(()
-				=> new Restaurant("Test", "Restaurant test", user));
+			var restaurant = new Restaurant(
+				 faker.Company.CompanyName(),
+				   faker.Company.CatchPhrase(),
+				   faker.Person.FullName,
+					 faker.Person.Email,
+					  faker.Phone.PhoneNumber());
+
+			var exception = Assert.Throws<DomainException>(() => restaurant.ChangeManager(user));
 
 			Assert.NotNull(exception);
 			Assert.IsType<DomainException>(exception);
